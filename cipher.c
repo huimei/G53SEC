cac9fb320a86a78b20701ea41168f3ec4c30fadb
Changhuan Lo <thisismade@live.com>
2016-04-24 00:04:38 +0800

    added readfile function

--- cipher.c
+++ cipher.c
@@ -1,106 +1,134 @@
-
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <ctype.h>
 
-#define MAXSIZE 1024
+#define STRSIZE 4096
 #define SHIFT 3
+#define INSIZE 50
 
-void encrypt(char*);
-void decrypt(char*);
+void encryptMsg(char*);
+void decryptMsg(char*);
+void readFile(const char*, char*);
 
-int main(void){
-	char c, choice[2], str[MAXSIZE];
-
-	while(1){
-		puts("To encrypt, input e or E\n");
- 		puts("To decrypt, input d or D\n");
- 		puts("To exit, input any other letter\n");
- 		puts("Your choice ->\n");
-
-		gets(choice);
-
-		if((choice[0]=='e')||(choice[0]=='E')){
-			puts("Please input text for encryption ->");
-			gets(str);
-			encrypt(str);
-		}else if((choice[0]=='d')||(choice[0]=='D')){
-			puts("Please input encrypted text for decryption ->");
-			gets(str);
-			decrypt(str);
-		}else{
-			break;
-		}
-	}
-	return 0;
+int main(int argc, char * argv[]) {
+    printf("=================== USAGE ===================\nTo encrypt a text file, e.g. abc.txt, input:\n");
+    printf("\tE abc.txt\nTo decrypt a text file, e.g. abc.txt, input:\n");
+    printf("\tD abc.txt\n=============================================\nYour input: ");
+    
+    char opt;
+    char str[STRSIZE];
+    char filename[INSIZE];
+    
+    if (scanf("%c %s", &opt, filename) < 0)
+    {
+        printf("Invalid arguments.\n");
+        exit(1);
+    }
+    opt = toupper(opt);
+    if (opt == 'E') {
+        readFile(filename, str);
+        encryptMsg(str);
+    } else if (opt == 'D') {
+        readFile(filename, str);
+        decryptMsg(str);
+    } else {
+        printf("Invalid argument.\n");
+        exit(1);
+    }
+    return 0;
 }
 
-void encrypt(char*str){
-	int length = strlen(str);
-	int shift = SHIFT;
-	int i;
-
-	for(i = 0; i < length; i++){
-		if (str[i] == " "){
+void encryptMsg(char * str) {
+    size_t length = strlen(str);
+    int shift = SHIFT;
+    int i;
+    
+    for(i = 0; i < length; i++){
+        if (str[i] == ' '){
             str[i] = str[i];
-		} else if(str[i]>=48 && str[i]<=57){
+        } else if(str[i]>=48 && str[i]<=57){
             str[i] = str[i] + shift;
             if(str[i]>57){
                 str[i] = 48 + (str[i] - 58);
             }else{
                 str[i] = str[i];
             }
-		} else if(str[i]>=65 && str[i]<=90){
+        } else if(str[i]>=65 && str[i]<=90){
             str[i] = str[i] + shift;
             if(str[i]>90){
                 str[i] = 65 + (str[i] - 91);
             }else{
                 str[i] = str[i];
             }
-		} else if(str[i]>=97 && str[i]<=122){
+        } else if(str[i]>=97 && str[i]<=122){
             str[i] = str[i] + shift;
             if(str[i]>122){
                 str[i] = 97 + (str[i] - 123);
             }else{
                 str[i] = str[i];
             }
-		}
-	}
-	printf("Encrypted: %s\n\n", str);
-	decrypt(str);
+        }
+    }
+    printf("\nEncrypted: %s\n\n", str);
+    decryptMsg(str);
 }
 
-void decrypt(char*str){
-	int length = strlen(str);
-	int shift = SHIFT;
-	int i;
-
-	for(i = 0; i < length; i++){
-		if (str[i] == " "){
+void decryptMsg(char * str){
+    size_t length = strlen(str);
+    int shift = SHIFT;
+    int i;
+    
+    for(i = 0; i < length; i++){
+        if (str[i] == ' '){
             str[i] = str[i];
-		} else if(str[i]>=48 && str[i]<=57){
+        } else if(str[i]>=48 && str[i]<=57){
             str[i] = str[i] - shift;
             if(str[i]<48){
                 str[i] = 58 - (48 - str[i]);
             }else{
                 str[i] = str[i];
             }
-		} else if(str[i]>=65 && str[i]<=90){
+        } else if(str[i]>=65 && str[i]<=90){
             str[i] = str[i] - shift;
             if(str[i]<65){
                 str[i] = 91 + (65 - str[i]);
             }else{
                 str[i] = str[i];
             }
-		} else if(str[i]>=97 && str[i]<=122){
+        } else if(str[i]>=97 && str[i]<=122){
             str[i] = str[i] - shift;
             if(str[i]<97){
                 str[i] = 123 - (97 - str[i]);
             }else{
                 str[i] = str[i];
             }
-		}
-	}
-	printf("Decrypted: %s\n\n", str);
+        }
+    }
+    printf("Decrypted: %s\n\n", str);
+}
+
+void readFile(const char * filename, char * str) {
+    FILE *fp;
+    long length;
+    char *buffer;
+    fp = fopen (filename,"r");
+    if(!fp) perror(filename),exit(1);
+    
+    fseek(fp,0L,SEEK_END);
+    length = ftell(fp);
+    rewind( fp );
+    
+    //allocate memory
+    buffer = calloc( 1, length+1 );
+    if( !buffer ) fclose(fp),fputs("memory alloc failed",stderr),exit(1);
+    
+    // copy file into buffer
+    if( 1!=fread( buffer , length, 1 , fp) )
+        fclose(fp),free(buffer),fputs("read failed",stderr),exit(1);
+    fclose(fp);
+    strcpy(str, buffer);
+    free(buffer);
 }
 
 
